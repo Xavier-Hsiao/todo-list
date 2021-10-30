731ebc88471d1495d11767584f0f2d738dca581f
Xavier-Hsiao <tim88776@gmail.com>
2021-10-30 12:47:09 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -69,6 +69,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(err => console.error(err))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
+})
+
 app.listen(3000, () => {
   console.log('The server is running on htpp://localhost:3000')
 })
