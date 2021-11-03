48c475cb852e36fcb6634cea0ac7ed9b892a311b
Xavier-Hsiao <tim88776@gmail.com>
2021-11-03 22:02:32 +0800

    feat: add sortr function

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
   Todo.find() //取出所有todo資料
     .lean() //將 Mongoose物件轉換成 JavaScrript陣列，這樣就不會再有save()之類的 Mongoose方法
+    .sort({_id: 'asc'})
     .then(todos => res.render('index', {todos}))
     .catch(err => console.error(err))
 })
